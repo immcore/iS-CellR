ad0ed12a6ae8a0e316ee5451b89eeda3fca53c0a
Mitul Patel <37114272+immcore@users.noreply.github.com>
2019-09-05 15:45:17 +0100

    Update global.R

--- global.R
+++ global.R
@@ -10,4 +10,4 @@ source("helpers.R") # Load all the code needed to show feedback on a button clic
 selectSteps <- list("Quality control and cell filtering", "Gene variability across single cells", "Linear dimensional reduction (PCA)", "Non-linear dimensional reduction (UMAP/tSNE)", "Differentially expressed genes", "Discriminating marker genes")
 
 # The max limit is 10GB when the app is run locally and 5MB when run from the server.
-if(Sys.getenv('SHINY_PORT') == "") options(shiny.maxRequestSize=10000*1024^2)
+options(shiny.maxRequestSize=10000*1024^2)
