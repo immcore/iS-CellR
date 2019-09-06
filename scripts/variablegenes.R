5b0b5ccbcef59cef09cb09b987cace933c008021
Mitul Patel <37114272+immcore@users.noreply.github.com>
2019-09-06 09:49:52 +0100

    Update variablegenes.R

--- scripts/variablegenes.R
+++ scripts/variablegenes.R
@@ -1,6 +1,12 @@
 #!/usr/bin/env Rscript
 
 # Detection of variable genes across the single cells
-VarGenes <- FindVariableGenes(object = scObject$val, mean.function = ExpMean, dispersion.function = LogVMR, 
-                          x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(scObject$val), 10)
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(scObject$val)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+CombinePlots(plots = list(plot1, plot2))
 
